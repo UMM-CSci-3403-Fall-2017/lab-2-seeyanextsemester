2dd374ca95f1c92490a2365feaf7dc13be0cc4d0
GLOBBIMUS <striker1342566@gmail.com>
2017-09-26 11:39:25 -0500

    Modify test to remove memory leaks

--- disemvowel/disemvowel_test.cpp
+++ disemvowel/disemvowel_test.cpp
@@ -1,47 +1,55 @@
 #include <gtest/gtest.h>
 
 #include "disemvowel.h"
-
+char* str;
 TEST(Disemvowel, HandleEmptyString) {
-  ASSERT_STREQ("", disemvowel((char*) ""));
+str = disemvowel((char*) "");
+      	ASSERT_STREQ("", str);
+	free(str);
 }
 
 TEST(Disemvowel, HandleNoVowels) {
-  ASSERT_STREQ("pqrst", disemvowel((char*) "pqrst"));
+ str = disemvowel((char*)"pqrst");
+	ASSERT_STREQ("pqrst",str);
+	free(str);
 }
 
 TEST(Disemvowel, HandleOnlyVowels) {
-  ASSERT_STREQ("", disemvowel((char*) "aeiouAEIOUOIEAuoiea"));
+str = disemvowel((char*) "aeiouAEIOUOIEAuoiea");
+	ASSERT_STREQ("", str);
+	free(str);
 }
 
 TEST(Disemvowel, HandleMorrisMinnesota) {
-  ASSERT_STREQ("Mrrs, Mnnst",
-		      disemvowel((char*) "Morris, Minnesota"));
+str = disemvowel((char*) "Morris, Minnesota");
+  ASSERT_STREQ("Mrrs, Mnnst", str);
+  free(str);
 }
 
 TEST(Disemvowel, HandlePunctuation) {
-  ASSERT_STREQ("n (nxplnd) lphnt!", 
-		      disemvowel((char*) "An (Unexplained) Elephant!"));
+str = disemvowel((char*) "An (Unexplained) Elephant!");
+  ASSERT_STREQ("n (nxplnd) lphnt!", str);
+free(str);
 }
 
 TEST(Disemvowel, HandleLongString) {
-  char* str;
+  char* string;
   int size;
   int i;
 
   size = 50000;
-  str = (char*) calloc(size, sizeof(char));
-  str[0] = 'x';
-  str[1] = 'y';
-  str[2] = 'z';
+  string = (char*) calloc(size, sizeof(char));
+  string[0] = 'x';
+  string[1] = 'y';
+  string[2] = 'z';
   for (i = 3; i < size-1; ++i) {
-    str[i] = 'a';
+    string[i] = 'a';
   }
-  str[size-1] = '\0';
-  
-  ASSERT_STREQ("xyz", disemvowel(str));
-
+  string[size-1] = '\0';
+  str = disemvowel(string);
+  ASSERT_STREQ("xyz", str);
   free(str);
+  free(string);
 }
 
 int main(int argc, char* argv[]) {
