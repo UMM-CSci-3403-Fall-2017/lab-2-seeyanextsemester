53aa424f26efe2b371eec734608a4e55ccfe1d35
GLOBBIMUS <striker1342566@gmail.com>
2017-09-26 11:36:49 -0500

    Wrote functions for removing vowels

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,41 @@
 #include <stdlib.h>
-
+#include <stdio.h>
 #include "disemvowel.h"
+#include <string.h>
+ 
+int isVowel(char character);
 
 char* disemvowel(char* str) {
-  return (char*) "";
+  int m = 0;
+  int len;
+  len = strlen(str);  
+  char* noVowels;
+  noVowels = (char*)calloc(len + 1,sizeof(char)); 
+
+  for(int i = 0;str[i] != '\0'; i++){
+   if(!(isVowel(str[i]))){
+     noVowels[m] = str[i];
+     m++;
+     }
+  }
+  noVowels[m] = '\0';
+  return noVowels;
+}
+
+int isVowel(char character){
+	switch(character){
+	    case 'a':
+            case 'e':
+            case 'u':
+            case 'i':
+            case 'o':
+            case 'A':
+	    case 'E':
+            case 'U':
+            case 'I':
+	    case 'O':
+            return 1;
+	    default:
+	    return 0;
+	}         
 }
